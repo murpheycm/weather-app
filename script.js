2fea8a473c0df0beabc1f71441822d78fc1e5f41
murpheycm <cmmurphey@gmail.com>
2023-10-30 08:16:05 -0400

    Defined variables

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+//store searched city
+var city="";
+
+var searchCity = $("#search-city");
+var cityList=[];
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWindSpeed=$("#wind-speed");
+var currentUvIndex= $("#uv-index");
 
