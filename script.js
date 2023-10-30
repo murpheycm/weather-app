784b66e80a92998d143fd2ad1210da4a5cfc04ad
murpheycm <cmmurphey@gmail.com>
2023-10-30 08:32:14 -0400

    Going a different route for scripting

--- script.js
+++ script.js
@@ -1,25 +1,62 @@
-//store searched city
-var city="";
-
-var searchCity = $("#search-city");
-var cityList=[];
-var searchButton = $("#search-button");
-var clearButton = $("#clear-history");
-var currentCity = $("#current-city");
-var temperature = $("#temperature");
-var humidty= $("#humidity");
-var windSpeed=$("#wind-speed");
-var uvIndex= $("#uv-index");
-
-//API key
-var APIKey="a0aca8a89948154a4182dcecc780b513";
-
-//Find available cities
-function find(c){
-    for (var i=0; i<cityList.length; i++){
-        if(c.toUpperCase()===cityList[i]){
-            return -1;
-        }
+document.addEventListener('DOMContentLoaded', function () {
+    const apiKey = 'a0aca8a89948154a4182dcecc780b513';
+    const searchForm = document.getElementById('sidebar');
+    const locationInput = document.getElementById('location');
+    const locationName = document.getElementById('userLocation');
+    const currentDate = document.getElementById('currentDate');
+    const locationTemp = document.getElementById('tempInputLoc');
+    const locationWind = document.getElementById('windInputLoc');
+    const locationHumidity = document.getElementById('humidityInputLoc');
+    const fiveDayForecast = document.getElementById('fiveDayForecast');
+  
+    searchForm.addEventListener('submit', function (e) {
+      e.preventDefault();
+      const city = locationInput.value;
+      const units = 'imperial';
+  
+      const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${apiKey}`;
+  
+      fetch(apiUrl)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          // Update HTML with the retrieved data
+          locationName.textContent = `Location: ${city}`;
+          currentDate.textContent = `Date: ${new Date().toLocaleDateString()}`;
+          locationTemp.textContent = `${data.list[0].main.temp} °C`;
+          locationWind.textContent = `${data.list[0].wind.speed} MPH`;
+          locationHumidity.textContent = `${data.list[0].main.humidity} %`;
+  
+          // Display the 5-day forecast
+          displayFiveDayForecast(data.list);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    });
+  
+    function displayFiveDayForecast(forecastData) {
+      fiveDayForecast.innerHTML = '';
+  
+      for (let i = 0; i < forecastData.length; i += 8) {
+        const forecast = forecastData[i];
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+          <h5 class="card-title">Date: ${new Date(forecast.dt * 1000).toLocaleDateString()}</h5>
+          <img class="card-img-top" src="https://example.com/your-image-url.jpg" alt="Weather Icon">
+          <ul class="card-body" style="list-style:none">
+            <li class="card-text">Temp: ${forecast.main.temp} °C</li>
+            <li class="card-text">Wind: ${forecast.wind.speed} MPH</li>
+            <li class="card-text">Humidity: ${forecast.main.humidity} %</li>
+          </ul>
+        `;
+  
+        fiveDayForecast.appendChild(card);
+      }
     }
-    return 1;
-}
+  });
