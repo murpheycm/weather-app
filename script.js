ee573e8fe6170ea6a255007e7607c8622e7ed70c
murpheycm <cmmurphey@gmail.com>
2023-10-30 08:19:20 -0400

    Added function to search cities

--- script.js
+++ script.js
@@ -6,10 +6,20 @@ var cityList=[];
 var searchButton = $("#search-button");
 var clearButton = $("#clear-history");
 var currentCity = $("#current-city");
-var currentTemperature = $("#temperature");
-var currentHumidty= $("#humidity");
-var currentWindSpeed=$("#wind-speed");
-var currentUvIndex= $("#uv-index");
+var temperature = $("#temperature");
+var humidty= $("#humidity");
+var windSpeed=$("#wind-speed");
+var uvIndex= $("#uv-index");
 
 //API key
 var APIKey="a0aca8a89948154a4182dcecc780b513";
+
+//Find available cities
+function find(c){
+    for (var i=0; i<cityList.length; i++){
+        if(c.toUpperCase()===cityList[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
