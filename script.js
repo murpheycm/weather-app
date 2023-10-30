822c7d0c920d0473b4c14f61a24cceda748139a7
murpheycm <124391666+murpheycm@users.noreply.github.com>
2023-10-30 11:24:25 -0400

    Trying a different script for rendering api info

--- script.js
+++ script.js
@@ -1,62 +1,68 @@
 $(document).ready(function (){
     const apiKey = '91ca803728e08fcbc1a0d92d460de95f';
-    const searchForm = document.getElementById('sidebar');
-    const locationInput = document.getElementById('location');
-    const locationName = document.getElementById('userLocation');
-    const currentDate = document.getElementById('currentDate');
-    const locationTemp = document.getElementById('tempInputLoc');
-    const locationWind = document.getElementById('windInputLoc');
-    const locationHumidity = document.getElementById('humidityInputLoc');
-    const fiveDayForecast = document.getElementById('fiveDayForecast');
-  
-    searchForm.addEventListener('submit', function (e) {
-      e.preventDefault();
-      const city = locationInput.value;
-      const units = 'imperial';
-  
-      const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${apiKey}`;
-  
-      fetch(apiUrl)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        })
-        .then((data) => {
-          // Update HTML with the retrieved data
-          locationName.textContent = `Location: ${city}`;
-          currentDate.textContent = `Date: ${new Date().toLocaleDateString()}`;
-          locationTemp.textContent = `${data.list[0].main.temp} °C`;
-          locationWind.textContent = `${data.list[0].wind.speed} MPH`;
-          locationHumidity.textContent = `${data.list[0].main.humidity} %`;
-  
-          // Display the 5-day forecast
-          displayFiveDayForecast(data.list);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
-    });
-  
-    function displayFiveDayForecast(forecastData) {
-      fiveDayForecast.innerHTML = '';
-  
-      for (let i = 0; i < forecastData.length; i += 8) {
-        const forecast = forecastData[i];
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.innerHTML = `
-          <h5 class="card-title">Date: ${new Date(forecast.dt * 1000).toLocaleDateString()}</h5>
-          <img class="card-img-top" src="https://example.com/your-image-url.jpg" alt="Weather Icon">
-          <ul class="card-body" style="list-style:none">
-            <li class="card-text">Temp: ${forecast.main.temp} °C</li>
-            <li class="card-text">Wind: ${forecast.wind.speed} MPH</li>
-            <li class="card-text">Humidity: ${forecast.main.humidity} %</li>
-          </ul>
-        `;
-  
-        fiveDayForecast.appendChild(card);
-      }
+    let searchForm;
+    let locationInput;
+    let locationName;
+    let currentDate;
+    let locationTemp;
+    let locationWind;
+    let locationHumidity;
+    let fiveDayForecast;
+    let searchHistory = [];
+
+    cities();
+
+    init();
+
+    //=================================================//
+
+    // Today's date
+    const momentDay = moment().format('dddd, MMMM Do');
+    $('.todayDate').prepend(momentDay);
+
+    // Generate dates for the 5-day forecast
+    for (var i = 1; i < 6; i++) {
+        $(`#${i}Date`).text(moment().add(i, 'd').format('dddd, MMMM Do'));
     }
-  });
+
+    // Add city
+    function addCity() {
+        $("#past-searches").prepend($("<button>").attr("type", "button").attr("data-city", city).addClass("past text-muted list-group-item list-group-item-action").text(city));
+        $("#search").val("");
+    }
+
+    // Has city been searched before?
+    function previousCity () {
+        if ( $(`#past-searches button[data-city="${city}"]`).length ) { 
+          $("#search").val("");
+        } else {
+          addCity();
+          savedCities.push(city);
+          localStorage.setItem("cities", JSON.stringify(savedCities))
+        }
+    }
+    
+    // Render cities
+    function renderCities() {
+        for (var i = 0; i < savedCities.length; i++) {
+          city = savedCities[i];
+          addCity();
+        }
+      }
+    
+    // Load Cities
+      function cities() {
+        var storedCities = JSON.parse(localStorage.getItem("cities"));
+        if (storedCities !== null) {
+          savedCities = storedCities;
+          renderCities();
+        } else {
+          city = "Richmond"
+          previousCity();
+        }
+      }
+
+    function init() {
+        getToday();
+    }
+});
